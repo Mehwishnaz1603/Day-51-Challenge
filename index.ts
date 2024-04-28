16d292c9787b3c095ab5d90886d1afbc47e1445c
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-29 00:08:47 +0500

    day 51 challenge

---
+++ index.ts
@@ -0,0 +1,24 @@
+            //🚀 Day 50 Challenge: Start Coding! 🚀 //
+
+// Read the following article: Top 10 Programming Tips For Beginners
+// https://www.geeksforgeeks.org/programming-tips-for-beginners/
+
+// 1. Apply Your Theoretical Knowledge Practically (Practice, Practice, Practice…)
+// 2. Don’t Leave a Gap in Your Learning
+// 3. Don’t do Over Analysis of the Concept
+// 4. Read The Error Message Carefully
+// 5. Solve the MCQs Questions in Programming
+// 6. Code With Hands
+// 8. Enhance Your Problem-Solving Skills
+// 9. Divide Things into Smaller Parts
+// 10. Find a Perfect Mentor
+// Conclusion
+// Remember that learning the first programming language always requires special effort. Programming also needs a lot of patience and practice. 
+// Initially, you will struggle and you will try to give up but that’s the moment when you need to have the patience to solve the questions. 
+
+// Your brain needs exercise to solve questions in programming and it will only come with practice. Start with smaller problems and train your
+// brain to solve simple problems first then move to the next level or some complex problem. You can’t solve the mathematics problems of the
+// 10th standard if your brain is not trained enough to solve the problems of the 9th standard. So don’t skip the question and move level by
+// level in coding. 
+
+
